df70469165975393d115dc0c820ad22c1c4e915a
SharkGaming <SharkGaming@SharkGaming-Pc>
2015-02-03 17:17:13 +0100

    Method added to john

--- src/cryptchat/JohnPreusObj.java
+++ src/cryptchat/JohnPreusObj.java
@@ -11,7 +11,7 @@ package cryptchat;
  */
 public class JohnPreusObj {
     private int numberOfSmokes = 1000;
-    
+    private final int kondiTal = 1;
     
     
     public JohnPreusObj(int numberOfSmokes)
