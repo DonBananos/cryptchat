6cc4d710a720c084e5b8eaceb14c3d8446845589
HM <HM@HM-PC>
2015-02-03 17:25:24 +0100

    Kogt lort

--- src/startApp/CryptChat.java
+++ src/startApp/CryptChat.java
@@ -1,9 +1,12 @@
+package startApp;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package cryptchat;
+
+import model.JohnPreusObj;
 
 /**
  *
